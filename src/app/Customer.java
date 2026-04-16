11594741637aa02a9b4f8c9202a198247afebe40
SunnyTany <46300377+SunnyTany@users.noreply.github.com>
2026-04-16 16:10:10 +0200

    init project

---
+++ src/app/Customer.java
@@ -0,0 +1,18 @@
+Copy code
+package app;
+
+public class Customer {
+
+    // Виправте декларацію змінних класу
+    private final name;
+    private final String phone;
+
+    // Виправте визначення конструктора
+    public Customer(name, String ) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    // Пропишіть нижче getters для всіх змінних класу
+
+}
